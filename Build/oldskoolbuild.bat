c283361e6995ea95a2c767e51824968e31d2cffd
NeonKnightOA <openarena.arg@gmail.com>
2022-03-29 21:55:18 -0300

    Yep, this is necessary.

--- Build/oldskoolbuild.bat
+++ Build/oldskoolbuild.bat
@@ -1,9 +1,14 @@
 @ECHO OFF
+
+REM Set to the default dir of UT according to v469, point to the right direction before using!
+SET uccdir=C:\Program Files\Unreal Tournament
+CD %uccdir%
+
 ECHO --- Deleting Old Files...
 del oldskool.u
 
 ECHO --- Compiling...
 REM This only works with UT v469
-ucc make -ini=oldskoolbuild.ini -bytehax
+%uccdir%\ucc.exe make -ini=oldskoolbuild.ini -bytehax
 
 ECHO --- All Done!
