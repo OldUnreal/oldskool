82d651a4c0d9dc22e28f9c2f99fbf0f9dfd3bcc3
Stijn Volckaert <stijnv@gmail.com>
2022-07-04 10:25:01 +0200

    Updated build files

--- Build/oldskoolbuild.bat
+++ Build/oldskoolbuild.bat
@@ -3,7 +3,8 @@ ECHO --- Deleting Old Files...
 del oldskool.u
 
 ECHO --- Compiling...
-REM This only works with UT v469
-ucc make -ini=oldskoolbuild.ini -bytehax
+REM This only works with UT v469c RC2 and upwards
+ucc make -ini=oldskoolbuild.ini -fixcompat -noconstchecks
+ucc conform oldskool.u oldskool.u.230
 
 ECHO --- All Done!
